9d22e1cea4c3ef3720aae9e328eae69c3d3876ac
Vova <he.beite.pliz@gmail.com>
2022-11-11 20:41:28 +0200

    Studio Polly
    Studio Polly

---
+++ js/burger.js
@@ -0,0 +1,13 @@
+
+const burger = document.querySelector('.burger')
+const burger__menu = document.querySelector('.burger__menu');
+const body = document.querySelector('.body');
+
+burger.addEventListener('click', function(){
+   burger.classList.toggle('active');
+   burger__menu.classList.toggle('active');
+   burger__menu.classList.toggle('none');
+   body.classList.toggle('lock');
+});
+
+  
---
+++ js/script.js
@@ -0,0 +1,20 @@
+﻿
+window.addEventListener('scroll', () => {
+   const scrolled = window.scrollY;
+   const header = document.querySelector('.header')
+
+   if (scrolled >= 10){
+      header.classList.add('scrol');
+   }
+   else if(scrolled < 10){
+      header.classList.remove('scrol');
+   }
+});
+
+
+const header__info = document.querySelector('.down');
+
+header__info.addEventListener('click', function(){
+   window.scrollTo(0,1282);
+});
+    
